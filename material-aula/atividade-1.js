6c286f0ece02888d1e27397ecbcfd10f43278274
N3th3rL0rd <hackerhel@gmail.com>
2023-09-22 16:29:23 -0300

    aked methods created and tested

--- material-aula/atividade-1.js
+++ material-aula/atividade-1.js
@@ -1,31 +1,86 @@
 // TO DO LIST (CRUD)
 
 class Atividade {
-    constructor(nome, descricao, responsavel) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.responsavel = responsavel;
-    }
+  constructor(nome, descricao, responsavel) {
+    this.nome = nome;
+    this.descricao = descricao;
+    this.responsavel = responsavel;
+  }
 }
 
 const listaDeAtividades = [];
 
+var listaAtividades = document.getElementById("listaAtividades");
+
+        // Função para exibir os itens do array na lista
+        function exibirItens(atividade) {
+          listaAtividades.innerHTML = ""; // Limpa a lista atual
+
+            for (var i = 0; i < listaDeAtividades.length; i++) {
+                var li = document.createElement("li");
+                // item.appendChild(document.createTextNode(listaDeAtividades[i]));
+                li.innerText = `Nome: ${atividade.nome} - Descrição: ${atividade.descricao} - Responsável: ${atividade.responsavel}`;
+                listaAtividades.appendChild(item);
+            }
+        }
+
+// const adicionarAtividadeHtml = (atividade) => {
+//   const listaAtividades = document.getElementById("listaAtividades");
+  // listaAtividades.innerHTML = "";
+  // const li = document.createElement("li");
+  // li.innerText = `Nome: ${atividade.nome} - Descrição: ${atividade.descricao} - Responsável: ${atividade.responsavel}`;
+  // listaAtividades.appendChild(li);
+// };
+
 //ADICIONAR UMA ATIVIDADE
 const adicionarAtividade = (nome, descricao, responsavel) => {
-    //logica de adicionar uma nova atividade
-}
+  nome = prompt("Informe o nome:");
+  descricao = prompt("Informe a descricao:");
+  responsavel = prompt("Informe o responsavel:");
+
+  const atividadeCadastrada = listaDeAtividades.find(
+    (atividade) => atividade.nome === nome
+  );
+  if (atividadeCadastrada) {
+    alert("Atividade já cadastrada!");
+    return;
+  }
+    const novaAtividade = new Atividade(nome, descricao, responsavel);
+    listaDeAtividades.push(novaAtividade);
+  // adicionarAtividadeHtml(novaAtividade);
+  exibirItens();
+  console.log(listaDeAtividades);
+};
 
 //REMOVER UMA ATIVIDADE
 const removerAtividade = (nome) => {
-    //logica de remover atividade
+  nome = prompt("Informe o nome da atividade:");
+  index = listaDeAtividades.findIndex((atividade) => {
+    return atividade.nome === nome;
+  });
+  listaDeAtividades.splice(index, 1);
+  // adicionarAtividadeHtml(listaDeAtividades);
+  exibirItens();
+  console.log(listaDeAtividades);
 }
 
 //PESQUISAR UMA ATIVIDADE
 const buscarAtividade = (nome) => {
-    //logica de buscar atividade
+  nome = prompt("Informe o nome da atividade:");
+  atividade = listaDeAtividades.find((atividade) => {
+    return atividade.nome === nome;
+  });
+  adicionarAtividadeHtml(atividade);
+  console.log(listaDeAtividades);
+  return atividade;
+
 }
 
 //EDITAR DESCRICAO DE UMA ATIVIDADE
 const editarDescricaoAtividade = (nome, novaDescricao) => {
-    //logica para editar descricao da atividade
-}
+  atividade = buscarAtividade(nome);
+  novaDescricao = prompt("Informe a nova descricao:");
+  atividade.descricao = novaDescricao;
+  adicionarAtividadeHtml(atividade);
+  console.log(listaDeAtividades);
+};
